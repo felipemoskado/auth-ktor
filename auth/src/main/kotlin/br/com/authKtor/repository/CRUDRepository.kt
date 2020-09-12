24fe3c77581f72ec1d92f0bbde31e7d9516843e5
Felipe Igawa Moskado <felipe.moskado@objective.com.br>
2020-09-12 20:06:47 -0300

    Adiciona insert e findAll genérico

--- auth/src/main/kotlin/br/com/authKtor/repository/CRUDRepository.kt
+++ auth/src/main/kotlin/br/com/authKtor/repository/CRUDRepository.kt
@@ -9,9 +9,14 @@ open class CRUDRepository<ID>(val table: TableImpl<*>, val tableId: TableField<o
 
     fun <T> query(block: DSLContext.() -> T): T = block(getDSLContext())
 
-    inline fun <reified RECORD> findById(id: ID): RECORD? = run {
+    inline fun <reified RECORD : Record> insert(record: RECORD) =
+        query { insertInto(table, *record.fields()).values(*record.valuesRow().fields()).execute() }
+
+    inline fun <reified RECORD> findById(id: ID): RECORD? =
         query { selectFrom(table).where(tableId.eq(id)).fetchSingleInto(RECORD::class.java) }
-    }
+
+    inline fun <reified RECORD : Record> findAll(): List<RECORD> =
+        query { selectFrom(table).fetchInto(RECORD::class.java) }
 
     private fun getDSLContext(): DSLContext = DSL.using(hikariDataSource, SQLDialect.POSTGRES)
 }
